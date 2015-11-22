503b8ac8b3c1948c1108ae7e5261aafea466ae1b
JayrAlencar <jayralencarpereira@gmail.com>
2015-11-21 21:43:41 -0300

    Versão 0.1.0

--- nesslab_reader.js
+++ nesslab_reader.js
@@ -3,17 +3,67 @@ var leitor = spawn(__dirname+'/Leitor.exe', []);
 var util = require('util');
 var events = require('events');
 
+
 nesslab_reader = function(){
 	var self = this;
 }
 
+var connected = false;
+
+
 nesslab_reader.prototype = new events.EventEmitter;
 
-//Methosd
+//Methods
 nesslab_reader.prototype.connect = function(ip, port){
 	leitor.stdin.write('connect\r\n');
 	leitor.stdin.write(ip+'\r\n'+port+'\r\n');
 	return this;
+}
+
+nesslab_reader.prototype.init = function(){
+	leitor.stdin.write('init\r\n');
+}
+
+nesslab_reader.prototype.stop = function(){
+	leitor.stdin.write('init\r\n');	
+}
+
+nesslab_reader.prototype.enableAntenna = function(antennaport){
+	leitor.stdin.write('setAntennaState\r\n');
+	leitor.stdin.write(antennaport+'\r\n');
+	leitor.stdin.write('1\r\n');
+}
+
+nesslab_reader.prototype.disableAntenna = function(antennaport){
+	leitor.stdin.write('setAntennaState\r\n');
+	leitor.stdin.write(antennaport+'\r\n');
+	leitor.stdin.write('0\r\n');
+}
+
+nesslab_reader.prototype.setPowerAntenna = function(antennaport, power){
+	leitor.stdin.write('setAntennaPower\r\n');
+	leitor.stdin.write(antennaport+'\r\n');
+	leitor.stdin.write(power+'\r\n');
+}
+
+nesslab_reader.prototype.getAntennaState = function(){
+	leitor.stdin.write('getAntennaState\r\n');
+}
+
+nesslab_reader.prototype.getPower = function(){
+	leitor.stdin.write('getPower\r\n');	
+}
+
+nesslab_reader.prototype.reconnect = function(){
+	leitor.stdin.write('reconnect\r\n');		
+}
+
+nesslab_reader.prototype.disconnect = function(){
+	leitor.stdin.write('disconnect\r\n');		
+}
+
+nesslab_reader.prototype.close = function(){
+	leitor.stdin.write('exit\r\n');	
 }
 
 leitor.stdout.on('data', function (data) {
@@ -21,16 +71,46 @@ leitor.stdout.on('data', function (data) {
 	var comando = data.toString();
 	var comandos = comando.split("|");
 	switch(comandos[0]){
+		case 'tag':
+			reader.emit('tag', comandos[1]);
+			break;
 		case 'waiting': 
 			reader.emit('waiting',"waiting for "+comandos[1]);
+			break;
+		case 'connected':
+			reader.emit('connect','Im connected with the reader');
+			break;
+		case 'initread':
+			reader.emit('init','Reading was initiated');
+			break;
 		case 'disconnected':
+			reader.emit('disconnected',comandos[1]);
+			console.log(comandos[1]);
+			break;
+		case 'timeout':
+			reader.emit('disconnected',comandos[1]);
+			console.log(comandos[1]);
+			break;
+		case 'antennastate':
+			var number = parseInt(comandos[1]);
+			var port1 = (number & 0x0001) > 0;
+            var port2 = (number & 0x0002) > 0;
+            var port3 = (number & 0x0004) > 0;
+            var port4 = (number & 0x0008) > 0;
+
+            reader.emit('stateport1',port1);
+            reader.emit('stateport2',port2);
+            reader.emit('stateport3',port3);
+            reader.emit('stateport4',port4);
+
+            break;
+
+        case 'antennapower':
+        	reader.emit('antennapower',comandos[1]);
+
 	}
 	// console.log('stdout: ' + data);
 });
 
-
-nesslab_reader.prototype.coisa = function(){
-	console.log("Coisa");
-}
 // util.inherits(nesslab_reader, EventEmitter);
 module.exports = new nesslab_reader();
---
+++ test.js
@@ -0,0 +1,19 @@
+var reader = require('./nesslab_reader');
+
+reader.connect('192.168.0.100', 5578);
+reader.on('connect', function(res){
+	console.log(res);
+	reader.init();
+});
+
+reader.on('init', function(res){
+	console.log(res);
+})
+
+reader.on('tag', function(tagId){
+	console.log(tagId);
+});
+
+reader.on('stateport4', function(res){
+	console.log(res)
+})
