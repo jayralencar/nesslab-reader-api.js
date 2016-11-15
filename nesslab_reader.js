862bebfbb4728e55bd827d144cda3b5df56e16f5
Jayr Alencar <jayralencarpereira@gmail.com>
2016-11-15 15:39:21 -0300

    getting response - power, state and init/stop reading

--- nesslab_reader.js
+++ nesslab_reader.js
@@ -1,27 +1,30 @@
 var events = require('events');
 var net = require('net');
 
-
 nesslab_reader = function(){
 	var self = this;
-	this.socket = new net.Socket();
-
+	this.socket.on('connect', function(res){
+		self.emit('connect',"The reader is connected");
+	});
 	this.socket.on('data', function(data){
+		var dataStr = data.toString();
+		var type = dataStr.substring(0,2);
+		console.log(dataStr);
 
-	});
-
-	this.socket.on('error', function(res){
-		console.log("Erro:"+res)
+		switch(type){
+			case '>p':
+				self.emit('power',parseInt(dataStr.substring(2)));
+				break;
+			case '>e':
+				self.emit('antennaState', parseInt(dataStr.substring(2)));
+				break;
+		}
 	})
-
-	this.socket.on('close', function(res){
-		console.log(res)
-	});
 }
 
-var connected = false;
-
 nesslab_reader.prototype = new events.EventEmitter;
+
+nesslab_reader.prototype.socket = net.Socket();
 
 // Node version
 nesslab_reader.prototype.nodeVersion = process.versions.node;
@@ -48,11 +51,7 @@ nesslab_reader.prototype.connect = function(ip, port, callback){
 
 	ip = ip || this.ip;
 	port = port || this.port;
-	this.socket = net.connect(port, ip, function(res){
-
-	}, function(res){
-		console.log("Erro:"+res)
-	});
+	this.socket.connect(port, ip);
 }
 
 nesslab_reader.prototype.init = function(){
@@ -60,7 +59,7 @@ nesslab_reader.prototype.init = function(){
 }
 
 nesslab_reader.prototype.stop = function(){
-	
+	this.socket.write(new Buffer([62,51,13,10]));
 }
 
 nesslab_reader.prototype.enableAntenna = function(antennaport){
@@ -75,12 +74,22 @@ nesslab_reader.prototype.setPowerAntenna = function(antennaport, power){
 	
 }
 
-nesslab_reader.prototype.getAntennaState = function(){
-	
+nesslab_reader.prototype.getAntennaState = function(callback){
+	this.socket.write(new Buffer([62,121,32,101,13,10]));
+	this.on('antennaState', function(data){
+		if(callback){
+			callback(data)
+		}
+	});
 }
 
-nesslab_reader.prototype.getPower = function(){
-	
+nesslab_reader.prototype.getPower = function(callback){
+	this.socket.write(new Buffer([62,121,32,112,13,10]));
+	this.on('power', function(data){
+		if(callback){
+			callback(data)
+		}
+	});
 }
 
 nesslab_reader.prototype.reconnect = function(){
--- test/app.js
+++ test/app.js
@@ -1,3 +1,10 @@
 var ness = require('../nesslab_reader');
 
-ness.connect();
+ness.connect('192.168.0.100',5578);
+
+ness.on('connect', function(res) {
+	ness.init();
+	setTimeout(function(){
+		ness.stop();
+	},10000);
+});
